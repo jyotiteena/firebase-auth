8d5038f34983ad7fef3b0bc184f0654ea5f6b96c
jyotiteena <jingarjyoti13@gmail.com>
2025-04-10 09:20:34 +0530

    store file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import Store from './features/store.js'
+import Store from './features/Store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={Store}>
